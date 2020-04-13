675994436b45aa5874e0366071449646a6f45007
Vichea <mr.vichea.007@gmail.com>
2020-04-14 01:45:33 +0700

    a

--- src/CrudServiceProvider.php
+++ src/CrudServiceProvider.php
@@ -2,20 +2,25 @@
 
 namespace Vichea\Crud;
 
+use App\Console\Commands\CrudApiCommand;
 use Illuminate\Support\ServiceProvider;
 
-class CrudServiceProvider extends ServiceProvider{
+class CrudServiceProvider extends ServiceProvider
+{
 
-    public function boot(){
+    public function boot()
+    {
 
         $this->publishes([
-            __DIR__."/resources/stubs" => "resources/stubs",
-            __DIR__."/Console/Commands/CrudApiCommand.php" => app_path("Console/Commands/CrudApiCommand.php"),
-        ]);
+            __DIR__ . "/resources/stubs" => "resources/stubs",
+            // __DIR__."/Console/Commands/CrudApiCommand.php" => app_path("Console/Commands/CrudApiCommand.php"),
+            $this->commands([
+                Console/Commands/CrudApiCommand::class,
+            ]);
+        ]); 
     }
 
-    public function register(){
-
+    public function register()
+    {
     }
 }
-
